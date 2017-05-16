8952f3f093a4ff73b07d7a09abd10d2c8a5d1e68
Shekhar Pankaj <shekharpankaj@outlook.com>
2017-05-16 17:52:12 +0530

    Changes layout
    Added Font awesome and some boiler plate codes for dashboard views

--- Me.Home/App_Start/BundleConfig.cs
+++ Me.Home/App_Start/BundleConfig.cs
@@ -14,6 +14,7 @@ namespace Me.Home
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -26,6 +27,13 @@ namespace Me.Home
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/font-awesome").Include(
+                "~/fonts/font-awesome-4.7.0/css/font-awesome.css"));
+
+
+            bundles.Add(new StyleBundle("~/bundles/LayoutViewStyles").Include(
+                "~/Content/Custom/Shared/Layout.css"));
+
         }
     }
 }
